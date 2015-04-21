aabe9afff5ebe46cf1ee1c2f1fc77fec5d0ecccc
DilanUA <dilan.u.ariyaratne@gmail.com>
2015-04-21 15:06:07 +0530

    Updated back-end email notification functionality

--- components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/email/EmailConstants.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/email/EmailConstants.java
@@ -20,9 +20,8 @@ package org.wso2.carbon.device.mgt.core.email;
 public final class EmailConstants {
 
     public static final class EnrolmentEmailConstants {
-        public static final String TITLE = "title";
         public static final String USERNAME = "user-name";
-        public static final String DOwN_LOAD_URL = "downloadUrl";
+        public static final String DOWNLOAD_URL = "downloadUrl";
         public static final String ENCODED_SCHEME = "UTF-8";
         public static final String PASSWORD = "password";
         public static final String FIRST_NAME = "first-name";

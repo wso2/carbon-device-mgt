aeed8b92a1be207368a3338fb25580c017430e85
harshanl <harshan@wso2.com>
2015-08-26 18:20:37 +0530

    Enabled adding custom permissions to permission-tree through webapps

--- components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/config/permission/lifecycle/WebAppDeploymentLifecycleListener.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/config/permission/lifecycle/WebAppDeploymentLifecycleListener.java
@@ -22,14 +22,31 @@ import org.apache.catalina.Lifecycle;
 import org.apache.catalina.LifecycleEvent;
 import org.apache.catalina.LifecycleListener;
 import org.apache.catalina.core.StandardContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.device.mgt.common.DeviceManagementException;
+import org.wso2.carbon.device.mgt.core.config.permission.PermissionManager;
+
+import javax.servlet.ServletContext;
+import java.io.File;
 
 @SuppressWarnings("unused")
 public class WebAppDeploymentLifecycleListener implements LifecycleListener {
+
+	private static final String PERMISSION_CONFIG_PATH = "META-INF" + File.separator + "permissions.xml";
+	private static final Log log = LogFactory.getLog(WebAppDeploymentLifecycleListener.class);
 
 	@Override
 	public void lifecycleEvent(LifecycleEvent lifecycleEvent) {
 		if (Lifecycle.AFTER_START_EVENT.equals(lifecycleEvent.getType())) {
 			StandardContext context = (StandardContext) lifecycleEvent.getLifecycle();
+			ServletContext servletContext = context.getServletContext();
+			try {
+				PermissionManager.getInstance().initializePermissions(servletContext.getResourceAsStream(PERMISSION_CONFIG_PATH));
+			} catch (DeviceManagementException e) {
+				log.error("Exception occurred while adding the permissions from webapp : "
+				          + servletContext.getContextPath(),e);
+			}
 		}
 	}
 

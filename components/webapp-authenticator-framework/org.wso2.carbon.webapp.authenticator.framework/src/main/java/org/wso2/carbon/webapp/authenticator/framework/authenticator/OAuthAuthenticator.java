b4d17b888b217335af06fd479b0c4510ff0f0bd5
harshanl <harshan@wso2.com>
2015-11-20 00:43:01 +0530

    fixed secondary user-store related issues & multi-tenancy issues

--- components/webapp-authenticator-framework/org.wso2.carbon.webapp.authenticator.framework/src/main/java/org/wso2/carbon/webapp/authenticator/framework/authenticator/OAuthAuthenticator.java
+++ components/webapp-authenticator-framework/org.wso2.carbon.webapp.authenticator.framework/src/main/java/org/wso2/carbon/webapp/authenticator/framework/authenticator/OAuthAuthenticator.java
@@ -112,9 +112,9 @@ public class OAuthAuthenticator implements WebappAuthenticator {
                 if (oAuth2TokenValidationResponseDTO.isValid()) {
                     String username = oAuth2TokenValidationResponseDTO.getAuthorizedUser();
                     //Remove the userstore domain from username
-                    if (username.contains("/")) {
+                    /*if (username.contains("/")) {
                         username = username.substring(username.indexOf('/') + 1);
-                    }
+                    }*/
                     authenticationInfo.setUsername(username);
                     authenticationInfo.setTenantDomain(MultitenantUtils.getTenantDomain(username));
                     authenticationInfo.setTenantId(Utils.getTenantIdOFUser(username));

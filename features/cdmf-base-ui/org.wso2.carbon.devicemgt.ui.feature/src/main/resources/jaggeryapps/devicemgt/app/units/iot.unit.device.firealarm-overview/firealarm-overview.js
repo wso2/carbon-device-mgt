8b8e201b433186a0a7e591d58091e98cf1b894e3
Ace <ruwany.ace@gmail.com>
2015-11-30 19:03:06 +0530

    optimizing device listing page

--- features/cdmf-base-ui/org.wso2.carbon.devicemgt.ui.feature/src/main/resources/jaggeryapps/devicemgt/app/units/iot.unit.device.firealarm-overview/firealarm-overview.js
+++ features/cdmf-base-ui/org.wso2.carbon.devicemgt.ui.feature/src/main/resources/jaggeryapps/devicemgt/app/units/iot.unit.device.firealarm-overview/firealarm-overview.js
@@ -2,7 +2,7 @@ function onRequest (context) {
     var log = new Log("detail.js");
     var deviceType = request.getParameter("type");
     var deviceId = request.getParameter("id");
-    log.info("###### loading overview section!");
+    log.info("###### loading Firealarm Overview!");
 
     if (deviceType != null && deviceType != undefined && deviceId != null && deviceId != undefined) {
         var deviceModule = require("/modules/device.js").deviceModule;
@@ -13,21 +13,11 @@ function onRequest (context) {
             var deviceInfo = device.properties.DEVICE_INFO;
             if (deviceInfo != undefined && String(deviceInfo.toString()).length > 0) {
                 deviceInfo = parse(stringify(deviceInfo));
-                if (device.type == "ios") {
-                    viewModel.imei = device.properties.IMEI;
-                    viewModel.phoneNumber = deviceInfo.PhoneNumber;
-                    viewModel.udid = deviceInfo.UDID;
-                } else if (device.type == "android") {
-                    viewModel.imei = device.properties.IMEI;
-                    viewModel.model = device.properties.DEVICE_MODEL;
-                    viewModel.vendor = device.properties.VENDOR;
-                } else if (device.type == "windows") {
-                    viewModel.imei = device.properties.IMEI;
-                    viewModel.model = device.properties.DEVICE_MODEL;
+                    viewModel.system = device.properties.IMEI;
+                    viewModel.machine = "Virtual Firealarm";
                     viewModel.vendor = device.properties.VENDOR;
                 }
                 device.viewModel = viewModel;
-            }
         }
         context.device = device;
 

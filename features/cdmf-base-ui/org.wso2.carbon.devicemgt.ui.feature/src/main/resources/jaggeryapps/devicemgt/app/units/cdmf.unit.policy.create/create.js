8bff5c1c597e177c53e3f002c80dfe47b3537fc6
Kasun Delgolla <kasun.mck@gmail.com>
2015-12-22 12:42:28 +0530

    Committing policy selection by type - fix

--- features/cdmf-base-ui/org.wso2.carbon.devicemgt.ui.feature/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.policy.create/create.js
+++ features/cdmf-base-ui/org.wso2.carbon.devicemgt.ui.feature/src/main/resources/jaggeryapps/devicemgt/app/units/cdmf.unit.policy.create/create.js
@@ -1,11 +1,16 @@
 function onRequest(context) {
     var userModule = require("/app/modules/user.js")["userModule"];
+    var utility = require('/app/modules/utility.js').utility;
     var response = userModule.getRoles();
     if (response["status"] == "success") {
         context["roles"] = response["content"];
     }
+
     var typesListResponse = userModule.getPlatforms();
     if (typesListResponse["status"] == "success") {
+        for (var type in typesListResponse["content"]) {
+            typesListResponse["content"][type]["icon"] = utility.getDeviceThumb(typesListResponse["content"][type]["name"]);
+        }
         context["types"] = typesListResponse["content"];
     }
     return context;

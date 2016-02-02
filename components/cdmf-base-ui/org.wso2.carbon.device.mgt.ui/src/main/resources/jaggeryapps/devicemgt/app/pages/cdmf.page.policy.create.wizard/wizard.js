5f3dc89a829e6538645fe1fcc65337a49a1dd0f1
charithag <charitha.ws@gmail.com>
2016-02-02 10:43:42 +0530

    Moved jaggery stuffs to components

---
+++ components/cdmf-base-ui/org.wso2.carbon.device.mgt.ui/src/main/resources/jaggeryapps/devicemgt/app/pages/cdmf.page.policy.create.wizard/wizard.js
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function onRequest(context){
+    context.handlebars.registerHelper('equal', function (lvalue, rvalue, options) {
+        if (arguments.length < 3)
+            throw new Error("Handlebars Helper equal needs 2 parameters");
+        if( lvalue!=rvalue ) {
+            return options.inverse(this);
+        } else {
+            return options.fn(this);
+        }
+    });
+    var deviceType = context.uriParams.deviceType;
+
+    return {"policyWizardUnitName" : "cdmf.unit.device.type." + deviceType + ".policy-wizard"};
+}
--- features/cdmf-base-ui/org.wso2.carbon.devicemgt.ui.feature/src/main/resources/jaggeryapps/devicemgt/app/pages/cdmf.page.policy.view/view.js
+++
@@ -1,4 +0,0 @@
-function onRequest(context) {
-    var deviceType = request.getParameter("type");
-    return {"deviceTypePolicyView" : "cdmf.unit.device.type." + deviceType + ".policy-view"};
-}

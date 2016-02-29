0923e0d2fed7f6ea1fa46cdea3d24a1931a9e88e
NuwanSameera <nuwansameera74@gmail.com>
2016-02-29 12:28:06 +0530

    Add logs when throw errors if debug enable.

--- components/certificate-mgt/org.wso2.carbon.certificate.mgt.core/src/main/java/org/wso2/carbon/certificate/mgt/core/impl/KeyStoreReader.java
+++ components/certificate-mgt/org.wso2.carbon.certificate.mgt.core/src/main/java/org/wso2/carbon/certificate/mgt/core/impl/KeyStoreReader.java
@@ -57,23 +57,33 @@ public class KeyStoreReader {
 
         } catch (KeyStoreException e) {
             String errorMsg = "KeyStore issue occurred when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (FileNotFoundException e) {
             String errorMsg = "KeyStore file not found when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (NoSuchAlgorithmException e) {
             String errorMsg = "Algorithm not found when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (CertificateException e) {
             String errorMsg = "CertificateException when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (IOException e) {
             String errorMsg = "Input output issue occurred when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } finally {
             try {
@@ -101,23 +111,33 @@ public class KeyStoreReader {
 
         } catch (KeyStoreException e) {
             String errorMsg = "KeyStore issue occurred when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (FileNotFoundException e) {
             String errorMsg = "KeyStore file not found when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (NoSuchAlgorithmException e) {
             String errorMsg = "Algorithm not found when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (CertificateException e) {
             String errorMsg = "CertificateException when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (IOException e) {
             String errorMsg = "Input output issue occurred when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } finally {
             try {
@@ -150,7 +170,9 @@ public class KeyStoreReader {
             caCertificate = keystore.getCertificate(ConfigurationUtil.getConfigEntry(ConfigurationUtil.CA_CERT_ALIAS));
         } catch (KeyStoreException e) {
             String errorMsg = "KeyStore issue occurred when loading KeyStore";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         }
 
@@ -171,15 +193,21 @@ public class KeyStoreReader {
                     ConfigurationUtil.getConfigEntry(ConfigurationUtil.KEYSTORE_CA_CERT_PRIV_PASSWORD).toCharArray()));
         } catch (UnrecoverableKeyException e) {
             String errorMsg = "Key is unrecoverable when retrieving CA private key";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (KeyStoreException e) {
             String errorMsg = "KeyStore issue occurred when retrieving CA private key";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (NoSuchAlgorithmException e) {
             String errorMsg = "Algorithm not found when retrieving CA private key";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         }
 
@@ -198,7 +226,9 @@ public class KeyStoreReader {
             raCertificate = keystore.getCertificate(ConfigurationUtil.getConfigEntry(ConfigurationUtil.RA_CERT_ALIAS));
         } catch (KeyStoreException e) {
             String errorMsg = "KeyStore issue occurred when retrieving RA private key";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         }
 
@@ -220,26 +250,26 @@ public class KeyStoreReader {
             }else {
                 String errorMsg = "NULL_CERT : No certificate found for the alias " + alias;
                 if(log.isDebugEnabled()){
-                    log.error(errorMsg);
+                    log.debug(errorMsg);
                 }
                 throw new KeystoreException(errorMsg);
             }
         } catch (CertificateManagementDAOException e) {
             String errorMsg = "Error when retrieving certificate the database for the alias " + alias;
             if(log.isDebugEnabled()){
-                log.error(errorMsg, e);
+                log.debug(errorMsg, e);
             }
             throw new KeystoreException(errorMsg, e);
         } catch (ClassNotFoundException | IOException e) {
             String errorMsg = "Error when deserializing saved certificate.";
             if(log.isDebugEnabled()){
-                log.error(errorMsg, e);
+                log.debug(errorMsg, e);
             }
             throw new KeystoreException(errorMsg, e);
         } catch (SQLException e) {
             String errorMsg = "Error when making a connection to the database.";
             if(log.isDebugEnabled()){
-                log.error(errorMsg, e);
+                log.debug(errorMsg, e);
             }
             throw new KeystoreException(errorMsg, e);
         } finally {
@@ -258,15 +288,21 @@ public class KeyStoreReader {
                     ConfigurationUtil.getConfigEntry(ConfigurationUtil.KEYSTORE_RA_CERT_PRIV_PASSWORD).toCharArray()));
         } catch (UnrecoverableKeyException e) {
             String errorMsg = "Key is unrecoverable when retrieving RA private key";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (KeyStoreException e) {
             String errorMsg = "KeyStore issue occurred when retrieving RA private key";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         } catch (NoSuchAlgorithmException e) {
             String errorMsg = "Algorithm not found when retrieving RA private key";
-            log.error(errorMsg, e);
+            if(log.isDebugEnabled()){
+                log.debug(errorMsg, e);
+            }
             throw new KeystoreException(errorMsg, e);
         }
 

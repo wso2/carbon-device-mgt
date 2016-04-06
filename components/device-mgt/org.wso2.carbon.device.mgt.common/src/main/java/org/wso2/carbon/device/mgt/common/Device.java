7174d000e0351f6dfe5fad09414ee1d3ef838527
Lakshman <lakshmanu@wso2.com>
2016-04-06 19:05:11 +0530

    override hash and equals function in Device to avoid duplicates in collections

--- components/device-mgt/org.wso2.carbon.device.mgt.common/src/main/java/org/wso2/carbon/device/mgt/common/Device.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.common/src/main/java/org/wso2/carbon/device/mgt/common/Device.java
@@ -147,4 +147,21 @@ public class Device implements Serializable{
                 "]";
     }
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Device))
+			return false;
+
+		Device device = (Device) o;
+
+		return getDeviceIdentifier().equals(device.getDeviceIdentifier());
+
+	}
+
+	@Override
+	public int hashCode() {
+		return getDeviceIdentifier().hashCode();
+	}
 }

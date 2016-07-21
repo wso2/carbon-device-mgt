250ce1f77c7f1329651ebc8b51588894c4643958
ayyoob <ayyoobhamza@gmail.com>
2016-07-21 20:14:43 +0530

    added device mgt grant types

---
+++ components/identity-extensions/org.wso2.carbon.device.mgt.oauth.extensions/src/main/java/org/wso2/carbon/device/mgt/oauth/extensions/constants.java
@@ -0,0 +1,13 @@
+package org.wso2.carbon.device.mgt.oauth.extensions;
+
+
+/**
+ * This hold the constants related oauth extensions.
+ */
+public class Constants {
+
+    public static final String DEFAULT_DEVICE_ASSERTION = "device";
+    public static final String DEFAULT_USERNAME_IDENTIFIER = "username";
+    public static final String DEFAULT_PASSWORD_IDENTIFIER = "password";
+
+}
---
+++ components/identity-extensions/org.wso2.carbon.device.mgt.oauth.extensions/src/main/java/org/wso2/carbon/device/mgt/oauth/extensions/handlers/grant/ExtendedDeviceMgtPasswordGrantHandler.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.device.mgt.oauth.extensions.handlers.grant;
+
+import com.google.gson.Gson;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.apimgt.keymgt.handlers.ExtendedPasswordGrantHandler;
+import org.wso2.carbon.device.mgt.oauth.extensions.Constants;
+import org.wso2.carbon.device.mgt.oauth.extensions.DeviceRequestDTO;
+import org.wso2.carbon.device.mgt.oauth.extensions.OAuthExtUtils;
+import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
+import org.wso2.carbon.identity.oauth2.model.RequestParameter;
+import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
+
+@SuppressWarnings("unused")
+public class ExtendedDeviceMgtPasswordGrantHandler extends ExtendedPasswordGrantHandler {
+
+    private static Log log = LogFactory.getLog(ExtendedDeviceMgtPasswordGrantHandler.class);
+
+    @Override
+    public boolean validateGrant(OAuthTokenReqMessageContext tokReqMsgCtx) throws IdentityOAuth2Exception {
+        RequestParameter parameters[] = tokReqMsgCtx.getOauth2AccessTokenReqDTO().getRequestParameters();
+        for (RequestParameter parameter : parameters) {
+            switch (parameter.getKey()) {
+                case Constants.DEFAULT_USERNAME_IDENTIFIER:
+                    String username = parameter.getValue()[0];
+                    tokReqMsgCtx.getOauth2AccessTokenReqDTO().setResourceOwnerUsername(username);
+                    break;
+
+                case Constants.DEFAULT_PASSWORD_IDENTIFIER:
+                    String password = parameter.getValue()[0];
+                    tokReqMsgCtx.getOauth2AccessTokenReqDTO().setResourceOwnerPassword(password);
+                    break;
+            }
+        }
+        return super.validateGrant(tokReqMsgCtx);
+    }
+
+    @Override
+    public boolean validateScope(OAuthTokenReqMessageContext tokReqMsgCtx) {
+        return OAuthExtUtils.validateScope(tokReqMsgCtx);
+    }
+
+}

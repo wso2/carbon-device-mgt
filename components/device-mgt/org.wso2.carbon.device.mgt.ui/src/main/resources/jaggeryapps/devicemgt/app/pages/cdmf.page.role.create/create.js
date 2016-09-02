0427a6dd4bbf925b0df5d7d008a0abb348a7d122
dilanua <dilan.u.ariyaratne@gmail.com>
2016-09-02 18:00:40 +0530

    Refactoring Role Create UI - moving units to page

--- components/device-mgt/org.wso2.carbon.device.mgt.ui/src/main/resources/jaggeryapps/devicemgt/app/pages/cdmf.page.role.create/create.js
+++ components/device-mgt/org.wso2.carbon.device.mgt.ui/src/main/resources/jaggeryapps/devicemgt/app/pages/cdmf.page.role.create/create.js
@@ -18,7 +18,7 @@
 
 /**
  * Returns the dynamic state to be populated by add-user page.
- *
+ * 
  * @param context Object that gets updated with the dynamic state of this page to be presented
  * @returns {*} A context object that returns the dynamic state of this page to be presented
  */

a61be7a7ec72a70bab6fb3a5fd2090353717a523
Rasika Perera <info.rasika@gmail.com>
2016-09-26 14:56:34 +0530

    Enable group management api

--- components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/service/api/admin/GroupManagementAdminService.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/service/api/admin/GroupManagementAdminService.java
@@ -18,82 +18,92 @@
  */
 package org.wso2.carbon.device.mgt.jaxrs.service.api.admin;
 
-import io.swagger.annotations.*;
-import org.wso2.carbon.apimgt.annotations.api.Scope;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.ResponseHeader;
+import org.wso2.carbon.apimgt.annotations.api.API;
+import org.wso2.carbon.apimgt.annotations.api.Permission;
 import org.wso2.carbon.policy.mgt.common.DeviceGroupWrapper;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.Date;
 
-//@Path("/admin/groups")
-//@Produces(MediaType.APPLICATION_JSON)
-//@Consumes(MediaType.APPLICATION_JSON)
-//@Api(value = "Group Management Administrative Service", description = "This an  API intended to be used by " +
-//        "'internal' components to log in as an admin user and do a selected number of operations. " +
-//        "Further, this is strictly restricted to admin users only ")
+@API(name = "Group Management Admin", version = "1.0.0", context = "/api/device-mgt/v1.0/admin/groups", tags = {"devicemgt_admin"})
+
+@Path("/admin/groups")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Api(value = "Group Management Administrative Service", description = "This an  API intended to be used by " +
+        "'internal' components to log in as an admin user and do a selected number of operations. " +
+        "Further, this is strictly restricted to admin users only ")
 public interface GroupManagementAdminService {
 
-//    @GET
-//    @ApiOperation(
-//            produces = MediaType.APPLICATION_JSON,
-//            httpMethod = "GET",
-//            value = "Get groups by the name.",
-//            notes = "Get devices the name of device and tenant.",
-//            response = DeviceGroupWrapper.class,
-//            responseContainer = "List",
-//            tags = "Group Management Administrative Service")
-//    @ApiResponses(value = {
-//            @ApiResponse(code = 200, message = "OK. \n Successfully fetched the list of groups.",
-//                    response = DeviceGroupWrapper.class,
-//                    responseContainer = "List",
-//                    responseHeaders = {
-//                            @ResponseHeader(
-//                                    name = "Content-Type",
-//                                    description = "The content type of the body"),
-//                            @ResponseHeader(
-//                                    name = "ETag",
-//                                    description = "Entity Tag of the response resource.\n" +
-//                                            "Used by caches, or in conditional requests."),
-//                            @ResponseHeader(
-//                                    name = "Last-Modified",
-//                                    description = "Date and time the resource has been modified the last time.\n" +
-//                                            "Used by caches, or in conditional requests."),
-//                    }),
-//            @ApiResponse(
-//                    code = 304,
-//                    message = "Not Modified. \n Empty body because the client has already the latest version of the requested resource."),
-//            @ApiResponse(
-//                    code = 406,
-//                    message = "Not Acceptable.\n The requested media type is not supported"),
-//            @ApiResponse(
-//                    code = 500,
-//                    message = "Internal Server ErrorResponse. \n Server error occurred while fetching the group list.")
-//    })
-//    @Permission(scope = "group-view", permissions = {"/permission/admin/device-mgt/user/groups/list"})
-//    Response getGroupsOfUser(
-//            @ApiParam(
-//                    name = "username",
-//                    value = "Username of the user.",
-//                    required = true)
-//            @QueryParam("username") String username,
-//            @ApiParam(
-//                    name = "If-Modified-Since",
-//                    value = "Timestamp of the last modified date",
-//                    required = false)
-//            @HeaderParam("If-Modified-Since") String timestamp,
-//            @ApiParam(
-//                    name = "offset",
-//                    value = "Starting point within the complete list of items qualified.",
-//                    required = false)
-//            @QueryParam("offset") int offset,
-//            @ApiParam(
-//                    name = "limit",
-//                    value = "Maximum size of resource array to return.",
-//                    required = false)
-//            @QueryParam("limit") int limit);
-//
-
-
+    @GET
+    @ApiOperation(
+            produces = MediaType.APPLICATION_JSON,
+            httpMethod = "GET",
+            value = "Get groups by the name.",
+            notes = "Get devices the name of device and tenant.",
+            response = DeviceGroupWrapper.class,
+            responseContainer = "List",
+            tags = "Group Management Administrative Service")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK. \n Successfully fetched the list of groups.",
+                         response = DeviceGroupWrapper.class,
+                         responseContainer = "List",
+                         responseHeaders = {
+                                 @ResponseHeader(
+                                         name = "Content-Type",
+                                         description = "The content type of the body"),
+                                 @ResponseHeader(
+                                         name = "ETag",
+                                         description = "Entity Tag of the response resource.\n" +
+                                                 "Used by caches, or in conditional requests."),
+                                 @ResponseHeader(
+                                         name = "Last-Modified",
+                                         description = "Date and time the resource has been modified the last time.\n" +
+                                                 "Used by caches, or in conditional requests."),
+                         }),
+            @ApiResponse(
+                    code = 304,
+                    message = "Not Modified. \n Empty body because the client has already the latest version of the " +
+                            "requested resource."),
+            @ApiResponse(
+                    code = 406,
+                    message = "Not Acceptable.\n The requested media type is not supported"),
+            @ApiResponse(
+                    code = 500,
+                    message = "Internal Server ErrorResponse. \n Server error occurred while fetching the group list.")
+    })
+    @Permission(name = "View All Groups", permission = "/permission/admin/device-mgt/user/groups/list")
+    Response getGroupsOfUser(
+            @ApiParam(
+                    name = "username",
+                    value = "Username of the user.",
+                    required = true)
+            @QueryParam("username") String username,
+            @ApiParam(
+                    name = "If-Modified-Since",
+                    value = "Timestamp of the last modified date",
+                    required = false)
+            @HeaderParam("If-Modified-Since") String timestamp,
+            @ApiParam(
+                    name = "offset",
+                    value = "Starting point within the complete list of items qualified.",
+                    required = false)
+            @QueryParam("offset") int offset,
+            @ApiParam(
+                    name = "limit",
+                    value = "Maximum size of resource array to return.",
+                    required = false)
+            @QueryParam("limit") int limit);
 }

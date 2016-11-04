2aa1c98d0b5240aced05ff17b4d4d1d7aa737275
Ace <ruwany.ace@gmail.com>
2016-11-04 19:12:06 +0530

    bringing common features to cdmf

---
+++ components/device-mgt/org.wso2.carbon.device.mgt.url.printer/src/main/java/org/wso2/carbon/device/mgt/url/printer/URLPrinterStartupHandler.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.device.mgt.url.printer;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.core.ServerStartupObserver;
+import org.wso2.carbon.device.mgt.url.printer.internal.UrlPrinterDataHolder;
+import org.wso2.carbon.utils.CarbonUtils;
+import org.wso2.carbon.utils.ConfigurationContextService;
+import org.wso2.carbon.utils.NetworkUtils;
+
+public class URLPrinterStartupHandler implements ServerStartupObserver {
+    private static final Log log = LogFactory.getLog(URLPrinterStartupHandler.class);
+
+    @Override
+    public void completingServerStartup() {
+    }
+
+    @Override
+    public void completedServerStartup() {
+        log.info("IoT Console URL : " + this.getServerUrl());
+    }
+
+    private String getServerUrl() {
+        // Hostname
+        String hostName = "localhost";
+        try {
+            hostName = NetworkUtils.getMgtHostName();
+        } catch (Exception ignored) {
+        }
+        // HTTPS port
+        String mgtConsoleTransport = CarbonUtils.getManagementTransport();
+        ConfigurationContextService configContextService =
+                UrlPrinterDataHolder.getInstance().getConfigurationContextService();
+        int port = CarbonUtils.getTransportPort(configContextService, mgtConsoleTransport);
+        int httpsProxyPort =
+                CarbonUtils.getTransportProxyPort(configContextService.getServerConfigContext(), mgtConsoleTransport);
+        if (httpsProxyPort > 0) {
+            port = httpsProxyPort;
+        }
+        return "https://" + hostName + ":" + port + "/devicemgt";
+    }
+}

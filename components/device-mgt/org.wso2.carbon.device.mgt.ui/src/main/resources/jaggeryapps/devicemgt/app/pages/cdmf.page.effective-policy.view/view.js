6d5a9d44baacdda95dfcef968c262a9219b47e8b
Supun94 <supunpramoda@gmail.com>
2016-11-08 09:26:15 +0530

    Adding cdmf effective policy page

---
+++ components/device-mgt/org.wso2.carbon.device.mgt.ui/src/main/resources/jaggeryapps/devicemgt/app/pages/cdmf.page.effective-policy.view/view.js
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+function onRequest(context) {
+    var utility = require("/app/modules/utility.js")["utility"];
+    var deviceType = context.uriParams.deviceType;
+    var deviceId = context.uriParams.deviceId;
+    new Log().info("Device Type : "+deviceType);
+    new Log().info("Device ID : "+deviceId);
+    return {"deviceTypePolicyView": utility.getTenantedDeviceUnitName(deviceType, deviceId,"policy-view")};
+}

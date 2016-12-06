161fe62a08d6ed8d0827852f66a86f8a67f4e779
charitha <charitha.ws@gmail.com>
2016-12-06 18:59:43 +0530

    Fix for https://wso2.org/jira/browse/IOTS-323

--- components/certificate-mgt/org.wso2.carbon.certificate.mgt.api/src/main/java/org/wso2/carbon/certificate/mgt/jaxrs/api/CertificateMgtService.java
+++ components/certificate-mgt/org.wso2.carbon.certificate.mgt.api/src/main/java/org/wso2/carbon/certificate/mgt/jaxrs/api/CertificateMgtService.java
@@ -1,19 +1,50 @@
 package org.wso2.carbon.certificate.mgt.jaxrs.api;
 
+import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import io.swagger.annotations.Authorization;
 import io.swagger.annotations.AuthorizationScope;
+import io.swagger.annotations.Extension;
+import io.swagger.annotations.ExtensionProperty;
+import io.swagger.annotations.Info;
+import io.swagger.annotations.SwaggerDefinition;
+import io.swagger.annotations.Tag;
 import org.wso2.carbon.apimgt.annotations.api.Scope;
 import org.wso2.carbon.certificate.mgt.jaxrs.beans.ErrorResponse;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+@SwaggerDefinition(
+        info = @Info(
+                version = "1.0.0",
+                title = "",
+                extensions = {
+                        @Extension(properties = {
+                                @ExtensionProperty(name = "name", value = "SCEP Management"),
+                                @ExtensionProperty(name = "context", value = "/api/device-mgt/v1.0/scep"),
+                        })
+                }
+        ),
+        tags = {
+                @Tag(name = "scep_management", description = "SCEP management related REST-API. " +
+                                                                    "This can be used to manipulated device " +
+                                                                    "certificate related details.")
+        }
+)
 @Path("/scep")
+@Api(value = "SCEP Management", description = "This API carries all device Certificate management " +
+                                                            "related operations.")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public interface CertificateMgtService {
 
     /**

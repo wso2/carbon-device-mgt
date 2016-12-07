b06a6b930fffdca213f590445a233efe6e221674
amalhub <amalg@wso2.com>
2016-12-07 17:30:38 +0530

    IOTS-292: Adding the api-mgt handler for gateway change

---
+++ components/apimgt-extensions/org.wso2.carbon.apimgt.handlers/src/main/java/org.wso2.carbon.apimgt.handlers/invoker/RESTConstants.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.apimgt.handlers.invoker;
+
+public class RESTConstants {
+    static String REST_CLIENT_CONFIG_ELEMENT = "restClientConfiguration";
+    static String REST_CLIENT_MAX_TOTAL_CONNECTIONS = "maxTotalConnections";
+    static String REST_CLIENT_MAX_CONNECTIONS_PER_ROUTE = "maxConnectionsPerRoute";
+    static String REST_CLEINT_CONNECTION_TIMEOUT = "connectionTimeout";
+    static String REST_CLEINT_SOCKET_TIMEOUT = "socketTimeout";
+}

c4ba14376e0cfb7cd46a3a7f17aedf4df4f29e18
madhawap <madhawa.818@gmail.com>
2017-05-04 15:32:25 +0530

    Removed commented lines

--- components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/service/impl/DeviceManagementServiceImpl.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/service/impl/DeviceManagementServiceImpl.java
@@ -550,7 +550,6 @@ public class DeviceManagementServiceImpl implements DeviceManagementService {
         if (policy == null) {
             deviceCompliance.setDeviceID(id);
             deviceCompliance.setComplianceData(null);
-            //deviceCompliance.setCode(0001l); //code 0001 means no compliance data related to the device
             return Response.status(Response.Status.OK).entity(deviceCompliance).build();
         } else {
             try {
@@ -559,7 +558,6 @@ public class DeviceManagementServiceImpl implements DeviceManagementService {
                         new DeviceIdentifier(id, type));
                 deviceCompliance.setDeviceID(id);
                 deviceCompliance.setComplianceData(complianceData);
-                //deviceCompliance.setCode(0002l); //code 0002 means there are compliance data related to the device
                 return Response.status(Response.Status.OK).entity(deviceCompliance).build();
             } catch (PolicyComplianceException e) {
                 String error = "Error occurred while getting the compliance data.";

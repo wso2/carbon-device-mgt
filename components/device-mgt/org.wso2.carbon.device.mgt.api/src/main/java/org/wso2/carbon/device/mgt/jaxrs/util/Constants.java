ddb8f4e9b3662e79206a4e895976a2c96ab38cdb
sinthuja <sinthuja@wso2.com>
2017-08-31 16:53:57 +0530

    Fixing https://github.com/wso2/product-iots/issues/353.

--- components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/util/Constants.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.api/src/main/java/org/wso2/carbon/device/mgt/jaxrs/util/Constants.java
@@ -30,6 +30,7 @@ public class Constants {
     public static final String DEFAULT_STREAM_VERSION = "1.0.0";
 	public static final String SCOPE = "scope";
 	public static final String JDBC_USERSTOREMANAGER = "org.wso2.carbon.user.core.jdbc.JDBCUserStoreManager";
+	public static final int DEFAULT_PAGE_LIMIT = 10;
 
 
 	public final class ErrorMessages {

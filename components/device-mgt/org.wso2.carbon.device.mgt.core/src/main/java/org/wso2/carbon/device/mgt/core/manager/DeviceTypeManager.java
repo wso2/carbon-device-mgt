d4615f80f44a3862b65fb5eca29c45aeedef8d85
Rasika Perera <info.rasika@gmail.com>
2017-11-15 12:46:14 +0530

    Implemented device type manager

--- components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/manager/DeviceTypeManager.java
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/manager/DeviceTypeManager.java
@@ -18,55 +18,19 @@
  */
 package org.wso2.carbon.device.mgt.core.manager;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.device.mgt.common.DeviceType;
 import org.wso2.carbon.device.mgt.common.exception.DeviceManagementException;
-import org.wso2.carbon.device.mgt.core.dao.DeviceManagementDAOException;
-import org.wso2.carbon.device.mgt.core.dao.DeviceManagementDAOFactory;
-import org.wso2.carbon.device.mgt.core.dao.DeviceTypeDAO;
 
-import java.sql.SQLException;
 import java.util.List;
-import java.util.regex.Pattern;
 
 /**
- * This class provides all functionalities for managing device types
+ * This interface provides device types
  */
-public class DeviceTypeManager {
-    private static final Log log = LogFactory.getLog(DeviceTypeManager.class);
-
-    private static final String DEVICETYPE_REGEX_PATTERN = "^[^ /]+$";
-    private static final Pattern patternMatcher = Pattern.compile(DEVICETYPE_REGEX_PATTERN);
-
-    private final DeviceTypeDAO deviceTypeDAO;
-
-    public DeviceTypeManager(DeviceTypeDAO deviceTypeDAO) {
-        this.deviceTypeDAO = deviceTypeDAO;
-    }
-
-    List<DeviceType> getDeviceTypes() throws DeviceManagementException {
-        if (log.isDebugEnabled()) {
-            log.debug("Get device types");
-        }
-        try {
-            DeviceManagementDAOFactory.openConnection();
-            return deviceTypeDAO.getDeviceTypes();
-        } catch (DeviceManagementDAOException e) {
-            String msg = "Error occurred while obtaining the device types";
-            log.error(msg, e);
-            throw new DeviceManagementException(msg, e);
-        } catch (SQLException e) {
-            String msg = "Error occurred while opening a connection to the data source";
-            log.error(msg, e);
-            throw new DeviceManagementException(msg, e);
-        } catch (Exception e) {
-            String msg = "Error occurred in getDeviceTypes";
-            log.error(msg, e);
-            throw new DeviceManagementException(msg, e);
-        } finally {
-            DeviceManagementDAOFactory.closeConnection();
-        }
-    }
-
+public interface DeviceTypeManager {
+    /**
+     * Get list of device types.
+     * @return list of device types
+     * @throws DeviceManagementException
+     */
+    List<DeviceType> getDeviceTypes() throws DeviceManagementException;
 }
---
+++ components/device-mgt/org.wso2.carbon.device.mgt.core/src/main/java/org/wso2/carbon/device/mgt/core/manager/DeviceTypeManagerImpl.java
@@ -0,0 +1,70 @@
+/*
+ *   Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *   WSO2 Inc. licenses this file to you under the Apache License,
+ *   Version 2.0 (the "License"); you may not use this file except
+ *   in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing,
+ *   software distributed under the License is distributed on an
+ *   "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *   KIND, either express or implied.  See the License for the
+ *   specific language governing permissions and limitations
+ *   under the License.
+ *
+ */
+package org.wso2.carbon.device.mgt.core.manager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.wso2.carbon.device.mgt.common.DeviceType;
+import org.wso2.carbon.device.mgt.common.exception.DeviceManagementException;
+import org.wso2.carbon.device.mgt.core.dao.DeviceManagementDAOException;
+import org.wso2.carbon.device.mgt.core.dao.DeviceManagementDAOFactory;
+import org.wso2.carbon.device.mgt.core.dao.DeviceTypeDAO;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/**
+ * This class provides all functionalities for managing device types
+ */
+public class DeviceTypeManagerImpl implements DeviceTypeManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DeviceTypeManagerImpl.class);
+
+    private static final String DEVICETYPE_REGEX_PATTERN = "^[^ /]+$";
+    private static final Pattern patternMatcher = Pattern.compile(DEVICETYPE_REGEX_PATTERN);
+
+    private final DeviceTypeDAO deviceTypeDAO;
+
+    public DeviceTypeManagerImpl(DeviceTypeDAO deviceTypeDAO) {
+        this.deviceTypeDAO = deviceTypeDAO;
+    }
+
+    public List<DeviceType> getDeviceTypes() throws DeviceManagementException {
+        LOGGER.debug("Get device types");
+        try {
+            DeviceManagementDAOFactory.openConnection();
+            return deviceTypeDAO.getDeviceTypes();
+        } catch (DeviceManagementDAOException e) {
+            String msg = "Error occurred while obtaining the device types";
+            LOGGER.error(msg, e);
+            throw new DeviceManagementException(msg, e);
+        } catch (SQLException e) {
+            String msg = "Error occurred while opening a connection to the data source";
+            LOGGER.error(msg, e);
+            throw new DeviceManagementException(msg, e);
+        } catch (Exception e) {
+            String msg = "Error occurred in getDeviceTypes";
+            LOGGER.error(msg, e);
+            throw new DeviceManagementException(msg, e);
+        } finally {
+            DeviceManagementDAOFactory.closeConnection();
+        }
+    }
+
+}

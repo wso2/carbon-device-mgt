583b17d40578e5835dfe8d8c6cb6a91f074ff835
Rasika Perera <info.rasika@gmail.com>
2018-03-15 23:02:16 +0530

    Add login.do, retry.do and consent.do
    This implements following fixes/improvements;
    * Adding an intermediate for user to accept consents.
    * Fixing issue of not redirected to the consent.do
    * Authentication failure page

---
+++ components/device-mgt/org.wso2.carbon.device.mgt.ui/src/main/resources/jaggeryapps/devicemgt/app/pages/cdmf.page.sign-in.consent-do/consent-do.js
@@ -0,0 +1,38 @@
+function onRequest(context) {
+    var Encode = Packages.org.owasp.encoder.Encode;
+    var viewModel = {};
+    viewModel.appName = Encode.forHtml(request.getParameter("sp"));
+    var mandatoryClaims = [];
+    var requestedClaims = [];
+
+    var mandatoryClaimsList, requestedClaimsList;
+    var i, j, partOne, partTwo;
+    if (request.getParameter("mandatoryClaims")) {
+        mandatoryClaimsList = request.getParameter("mandatoryClaims").split(",");
+        for (j = 0; j < mandatoryClaimsList.length; j++) {
+            var mandatoryClaimsStr = mandatoryClaimsList[j];
+            i = mandatoryClaimsStr.indexOf('_');
+            partOne = mandatoryClaimsStr.slice(0, i);
+            partTwo = mandatoryClaimsStr.slice(i + 1, mandatoryClaimsStr.length);
+            mandatoryClaims.push(
+                {"claimId": Encode.forHtmlAttribute(partOne), "displayName": Encode.forHtmlAttribute(partTwo)}
+            );
+        }
+    }
+    if (request.getParameter("requestedClaims")) {
+        requestedClaimsList = request.getParameter("requestedClaims").split(",");
+        for (j = 0; j < requestedClaimsList.length; j++) {
+            var requestedClaimsStr = requestedClaimsList[j];
+            i = requestedClaimsStr.indexOf('_');
+            partOne = requestedClaimsStr.slice(0, i);
+            partTwo = requestedClaimsStr.slice(i + 1, requestedClaimsStr.length);
+            requestedClaims.push(
+                {"claimId": Encode.forHtmlAttribute(partOne), "displayName": Encode.forHtmlAttribute(partTwo)}
+            );
+        }
+    }
+    viewModel.mandatoryClaims = mandatoryClaims;
+    viewModel.requestedClaims = requestedClaims;
+    viewModel.sessionDataKey = Encode.forHtmlAttribute(request.getParameter("sessionDataKey"));
+    return viewModel;
+}
